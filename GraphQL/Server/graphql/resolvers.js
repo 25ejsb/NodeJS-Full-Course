f0cb0a2110baeec39ebd8f21f5a7003028e8bcdf
25ejsb <eitan.brochstein@icloud.com>
2024-03-17 19:36:25 -0400

    lol

--- GraphQL/Server/graphql/resolvers.js
+++ GraphQL/Server/graphql/resolvers.js
@@ -1,3 +1,55 @@
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
+const validator = require("validator")
+const jwt = require("jsonwebtoken")
+
+// default objects return are args, and req
 module.exports = {
-    
+    createUser: async function({userInput}, req) {
+        const errors = [];
+        if (!validator.isEmail(userInput.email)) {
+            errors.push({message: "Email is invalid"})
+        }
+        if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, {min: 5})) {
+            errors.push({message: "Password is too short!"})
+        }
+        if (errors.length > 0) {
+            const error = new Error("Invalid input")
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+        const existingUser = await User.findOne({email: userInput.email})
+        if (existingUser) {
+            const error = new Error("User exists already!")
+            throw error;
+        }
+        const hashedPw = await bcrypt.hash(userInput.password, 12);
+        const user = new User({
+            email: userInput.email,
+            name: userInput.name,
+            password: hashedPw
+        })
+        const createdUser = await user.save()
+        return {...createdUser._doc, _id: createdUser._id.toString() }
+    },
+    login: async function({email, password}) {
+        const user = await User.findOne({email: email})
+        if (!user) {
+            const error = new Error("User not found!")
+            error.code = 401
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password)
+        if (!isEqual) {
+            const error = new Error("Password is not correct")
+            error.code = 401;
+            throw error;
+        }
+        const token = jwt.sign({
+            userId: user._id.toString(),
+            email: user.email,
+        }, "somesuperdupersecret", {expiresIn: "1h"})
+        return {token: token, userId: user._id.toString()}
+    }
 }
