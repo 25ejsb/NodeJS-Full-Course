844ad5654d7d26c5f706c579f1d16de25179d6e7
25ejsb <eitan.brochstein@icloud.com>
2024-04-25 14:00:11 -0400

    lol

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -2,6 +2,9 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button")!; // ! means could be null
 
+const numResults: number[] = [];
+const stringResults: string[] = [];
+
 function add(num1: number | string, num2: number | string) {
     if (typeof num1 == 'number' && typeof num2 == 'number') {
         return num1 + num2;
@@ -20,8 +23,11 @@ buttonElement.addEventListener("click", () => {
     const num2 = num2Element.value;
     // + turns the string into integer
     const result = add(+num1, +num2);
+    numResults.push(result as number)
     const stringResult = add(num1, num2);
+    stringResults.push(stringResult as string);
     console.log(stringResult);
     console.log(result);
     printResult({val: result as number, timestamp: new Date()})
+    console.log(numResults, stringResults)
 })
