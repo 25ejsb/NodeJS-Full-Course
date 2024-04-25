5d9912fa0ffdeb215ab1c12c71f01e52071b01d1
25ejsb <eitan.brochstein@icloud.com>
2024-04-25 13:56:52 -0400

    lol

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -3,12 +3,24 @@ const num1Element = document.getElementById("num1");
 const num2Element = document.getElementById("num2");
 const buttonElement = document.querySelector("button"); // ! means could be null
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 == 'number' && typeof num2 == 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 == 'string' && typeof num2 == 'string') {
+        return num1 + " " + num2;
+    }
+    return +num1 + +num2;
+}
+function printResult(resultObj) {
+    console.log(resultObj.val);
 }
 buttonElement.addEventListener("click", () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     // + turns the string into integer
     const result = add(+num1, +num2);
+    const stringResult = add(num1, num2);
+    console.log(stringResult);
     console.log(result);
+    printResult({ val: result, timestamp: new Date() });
 });
--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -2,8 +2,17 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button")!; // ! means could be null
 
-function add(num1: number, num2: number) {
-    return num1 + num2;
+function add(num1: number | string, num2: number | string) {
+    if (typeof num1 == 'number' && typeof num2 == 'number') {
+        return num1 + num2;
+    } else if (typeof num1 == 'string' && typeof num2 == 'string') {
+        return num1 + " " + num2;
+    }
+    return +num1 + +num2;
+}
+
+function printResult(resultObj: { val: number; timestamp: Date }) {
+    console.log(resultObj.val);
 }
 
 buttonElement.addEventListener("click", () => {
@@ -11,5 +20,8 @@ buttonElement.addEventListener("click", () => {
     const num2 = num2Element.value;
     // + turns the string into integer
     const result = add(+num1, +num2);
+    const stringResult = add(num1, num2);
+    console.log(stringResult);
     console.log(result);
+    printResult({val: result as number, timestamp: new Date()})
 })
