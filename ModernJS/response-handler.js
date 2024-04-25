0ae6885a9acf1e7ab00f59e3ba2ccb9e0803c5af
25ejsb <eitan.brochstein@icloud.com>
2024-04-24 21:20:10 -0400

    lol

--- ModernJS/response-handler.js
+++ ModernJS/response-handler.js
@@ -1,4 +1,4 @@
-import fs from "fs"
+import fs from "fs/promises"
 import path, { dirname } from "path"
 import {fileURLToPath} from "url"
 
@@ -6,8 +6,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename)
 
 export const resHandler = (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'my-page.html'))
+    fs.readFile("my-page.html", "utf-8").then((data) => {
+        res.send(data)
+    }).catch(err => {
+        console.log(err)
+    })
+    //res.sendFile(path.join(__dirname, 'my-page.html'))
 }
-
 // this export default would be the one output of this script
 // export default resHandler;
